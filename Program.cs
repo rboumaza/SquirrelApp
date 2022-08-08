6f34b195e345f6ce11b3428ff066ef8fcbc86d4e
rboumaza <rachid.boumaza@laposte.net>
2022-08-08 16:12:22 +0200

    V1

--- Program.cs
+++ Program.cs
@@ -23,11 +23,44 @@ namespace SquirrelApp
             //    await mgr.UpdateApp();
             //}
 
-            using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/rboumaza/SquirrelApp"))
-            {
-                var release = await mgr.Result.UpdateApp();
-            }
+            //using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/rboumaza/SquirrelApp"))
+            //{
+            //    var release = await mgr.Result.UpdateApp();
+            //}
+
+            await Update();
             Application.Run(new Form1());
+        }
+
+
+        private static async Task Update()
+        {
+            try
+            {
+                using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/rboumaza/SquirrelApp"))
+                {
+                    if (mgr.Result.IsInstalledApp)
+                    {
+                        MessageBox.Show($"Current Version: {mgr.Result.CurrentlyInstalledVersion()}");
+                        var updates = await mgr.Result.CheckForUpdate();
+                        if (updates.ReleasesToApply.Any())
+                        {
+                            MessageBox.Show("Updates found. Applying updates.");
+                            var release = await mgr.Result.UpdateApp();
+
+                            //MessageBox.Show(CleanReleaseNotes(release.GetReleaseNotes(Path.Combine(mgr.RootAppDirectory, "packages"))),
+                            //$"Casual Meter Update - v{release.Version}");
+
+                            MessageBox.Show("Updates applied. Restarting app.");
+                            //UpdateManager.RestartApp();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {   //log exception and move on
+                MessageBox.Show(e.Message);
+            }
         }
     }
 }
